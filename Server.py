c22488b11ba3ca2b57543a21db0601f5e9740265
ekremcet <ekrem.cetinkaya@ozu.edu.tr>
2019-05-15 22:47:31 +0300

    changed structure to websocket

--- Server.py
+++ Server.py
@@ -1,56 +1,43 @@
-import socket
+import sys
 import string
 import random
 import time
-from _thread import start_new_thread
+from websocket_server import WebsocketServer
 
-FILE_SIZE = 25000000
-BUFFER_SIZE = 4096
+FILE_SIZE = 10000000
 
 
 class Server:
     def __init__(self, ip_addr, port):
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock = WebsocketServer(port=port, host=ip_addr)
         self.server_addr = ip_addr
-        self.generate_file()
+        #self.generate_file()
 
     def generate_file(self):
-        # Generate 25 MB file with random strings in each run
+        # Generate 10 MB file with random strings in each run
         # This is done to prevent caching
         data = self.generate_random_data(FILE_SIZE)
-        with open("./downfile", "wb") as f:
-            f.write(data.encode("ascii"))
+        with open("./downfile", "w") as f:
+            f.write(data)
 
-    def start_server(self):
-        self.sock.bind((self.server_addr, self.port))
-        self.sock.listen(5)
-        while True:
-            print("Waiting for connections..")
-            client, client_addr = self.sock.accept()
-            print("Connected to: " + str(client_addr[0]) + ":" + str(client_addr[1]))
-            start_new_thread(self.client_thread, (client, ))
+    def start_server(self, type):
+        if type == "D":
+            self.sock.set_fn_new_client(self.download_thread)
+        elif type == "U":
+            self.sock.set_fn_new_client(self.upload_thread)
+        elif type == "P":
+            self.sock.set_fn_new_client(self.delay_thread)
+        self.sock.run_forever()
 
     def get_delay(self, client):
         # Client sends a packet to server, server sends back a packet as soon as it receives that packet
         # Time passed between 2 received packets are calculated and divided by two to obtain RTT
         # This process is repeated 10 times to obtain more reliable results
         print("Delay Test")
-        rtt = 0.0
-        for _ in range(10):
-            rtt += self.get_pings(client)
-        ping = rtt / 20
-        client.send(str(round(ping)).encode("ascii"))
+        self.sock.set_fn_message_received(self.get_ping)
 
-    @staticmethod
-    def get_pings(client):
-        t1 = time.time()
-        msg = client.recv(BUFFER_SIZE)
-        client.send(msg)
-        t2 = time.time()
-
-        return (t2 - t1) * 1000
+    def get_ping(self, client, server, message):
+        self.sock.send_message(client, self.generate_random_data(64))
 
     @staticmethod
     def generate_random_data(size):
@@ -59,48 +46,43 @@ class Server:
 
         return ''.join(random.choice(alphabet) for _ in range(size))
 
-    @staticmethod
-    def get_download_speed(client):
-        # Send a 25MB randomly generated file to client
+    def get_download_speed(self, client):
+        # Send a 10MB randomly generated file to client
         # Speed calculation is done in client side
         print("Download Test")
         download_file = open("./downfile", "rb")
         buff = download_file.read()
         # Send the file to client
         while buff:
-            client.send(buff)
+            self.sock.send_message(client, buff)
             buff = download_file.read()
-
+        print("Test Complete")
         download_file.close()
 
-    @staticmethod
-    def get_upload_speed(client):
-        # Receive a 25MB randomly generated file from client
-        # Speed calculation is done in server side and then sent to client
+    def upload_control(self, client, server, message):
+        self.bytes_received += len(message)
+        if self.bytes_received >= FILE_SIZE:
+            t2 = time.time()
+            self.sock.send_message(client, str(t2 - self.t1))
+
+    def get_upload_speed(self, client):
+        # Receive a 10MB randomly generated data from client
+        # Speed calculation is done in client side
         print("Upload Test")
-        bytes_recieved = 0
-        t1 = time.time()
-        while True:
-            data = client.recv(BUFFER_SIZE)
-            bytes_recieved += len(data)
-            if bytes_recieved >= FILE_SIZE:
-                break
+        self.bytes_received = 0
+        self.t1 = time.time()
+        self.sock.set_fn_message_received(self.upload_control)
 
-        t2 = time.time()
-        upload_speed = str(round(((FILE_SIZE / (t2 - t1)) * 0.000001) * 8))
-        client.send(str(upload_speed).encode("ascii"))
+    def delay_thread(self, client, server):
+        self.get_delay(client)
 
-    def client_thread(self, client):
-        # First 4 bytes in connection is the test type
-        request = client.recv(4).decode("ascii")
-        requests = {"Ping": self.get_delay,
-                    "Down": self.get_download_speed,
-                    "Upld": self.get_upload_speed}
-        requests[request](client)
-        client.shutdown(socket.SHUT_RDWR)
-        client.close()
+    def download_thread(self, client, server):
+        self.get_download_speed(client)
+
+    def upload_thread(self, client, server):
+        self.get_upload_speed(client)
 
 
 if __name__ == '__main__':
-    server = Server("0.0.0.0", 33333)
-    server.start_server()
+    upload_server = Server("0.0.0.0", int(sys.argv[1]))
+    upload_server.start_server(type=sys.argv[2])
